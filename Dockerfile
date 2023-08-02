67c9d68108c0a9f85f2de0ab5ba91e4b35527dd5
Isaac Gray-Christensen <christensenisaac1995@gmail.com>
2023-08-02 07:48:47 -0700

    Initial

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+FROM ubuntu:22.04
+
+ARG PAPERCUT_MAJOR_VER=21.x
+ARG PAPERCUT_VERSION=21.0.4.57587
+
+ARG MYSQL_CONNECTOR_VERSION=8.0.30
+ARG MYSQL_CONNECTOR_DOWNLOAD_URL=https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-${MYSQL_CONNECTOR_VERSION}.tar.gz
+
+# WORKDIR /papercut
+
+RUN apt update; apt install -y --no-install-recommends wget cpio cups ca-certificates ;\
+    useradd -mUd /papercut -s /bin/bash papercut; \
+    echo "papercut - nofile 65535" >> /etc/security/limits.conf; \
+    wget https://cdn1.papercut.com/web/products/ng-mf/installers/mf/${PAPERCUT_MAJOR_VER}/pcmf-setup-${PAPERCUT_VERSION}.sh -O pcmf-setup.sh; \
+    chmod a+rx pcmf-setup.sh ; \
+    runuser -l papercut -c "/pcmf-setup.sh -v --non-interactive" ; \
+    rm -f pcmf-setup.sh ; \
+    /papercut/MUST-RUN-AS-ROOT ; \
+    /etc/init.d/papercut stop ;\
+    /etc/init.d/papercut-web-print stop ;\
+    wget ${MYSQL_CONNECTOR_DOWNLOAD_URL} -O mysql.tar.gz ;\
+    tar -xzvf mysql.tar.gz -C / ;\
+    rm mysql.tar.gz ;\ 
+    mv /mysql-connector-java-${MYSQL_CONNECTOR_VERSION}/mysql-connector-java-${MYSQL_CONNECTOR_VERSION}.jar /papercut/server/lib-ext/ ;\
+    rm -r /papercut/mysql-connectechor-java-${MYSQL_CONNECTOR_VERSION} ;\
+    chown -R papercut:papercut /papercut ;\
+    chmod +x /papercut/server/bin/linux-x64/setperms ;\
+    /papercut/server/bin/linux-x64/setperms ;\
+    runuser -l papercut -c "/papercut/server/bin/linux-x64/db-tools init-db -f -q"
+
+
+EXPOSE 9191 \
+        9192 \
+        9193
+
+ENTRYPOINT [ "/etc/init.d/papercut" ]
+CMD [ "console" ]
---
+++ image_setup.py
@@ -0,0 +1,103 @@
+import requests
+
+### Default vars/config used to setup image
+
+# Setup Page 1 (user = admin, and password = password)
+# service=direct/1/SetupAdmin/$Form &\
+# sp=S0 &\
+# Form0=password,passwordVerify,$PropertySelection,$Submit &\
+# password=password &\
+# passwordVerify=password &\
+# $PropertySelectio= &\
+# $Submit=Next
+payload_page1 = {
+    'service': 'direct/1/SetupAdmin/$Form',
+    'sp': 'S0',
+    'Form0': 'password,passwordVerify,$PropertySelection,$Submit',
+    'password': 'password',
+    'passwordVerify': 'password',
+    '$PropertySelectio': '',
+    '$Submit': 'Next'
+}
+
+# Setup Page 2 - Print cost
+# service=direct/1/SetupOrgType/$Form &\
+# sp=S0 &\
+# Form0=$RadioGroup,$Submit,$Submit$0 &\
+# $RadioGroup=0 &\
+# $Submit=Next
+payload_page2 = {
+    'service': 'direct/1/SetupOrgType/$Form',
+    'sp': 'S0',
+    'Form0': '$RadioGroup,$Submit,$Submit$0',
+    '$RadioGroup': '0',
+    '$Submit': 'Next'
+}
+
+# Setup Page 3 - SetupUserCredit
+# service=direct/1/SetupPrintCost/$Form &\
+# sp=S0 &\
+# Form0=defaultColorPageCost,defaultGrayscalePageCost,$Submit,$Submit$0 &\
+# defaultColorPageCost=$0.00 &\
+# defaultGrayscalePageCost=$0.00 &\
+# $Submit=Next
+payload_page3 = {
+    'service': 'direct/1/SetupPrintCost/$Form',
+    'sp': 'S0',
+    'Form0': 'defaultColorPageCost,defaultGrayScalePageCost,$Submit,$Submit$0',
+    'defaultColorPageCost': '$0.00',
+    'defaultGrayScalePageCost': '$0.00',
+    '$Submit': 'Next'
+}
+
+# Setup Page 4 - SetupUserSource
+# service=direct/1/SetupUserCredit/$Form &\
+# sp=S0 &\
+# Form0=initialCredit,restricted,$Submit,$Submit$0 &\
+# initialCredit=$0.00 &\
+# $Submit=Next
+payload_page4 = {
+    'service': 'direct/1/SetupUserCredit/$Form',
+    'sp': 'S0',
+    'Form0': 'initialCredit,restricted,$Submit,$Submit$0',
+    'initialCredit': '$0.00',
+    '$Submit': 'Next'
+}
+
+# Setup Page 5 - SetupVerify
+# service=direct/1/SetupUserSource/$Form &\
+# sp=S0 &\
+# Form0=$RadioGroup,$Select,$LinkSubmit,$Submit,$Submit$0 &\
+# $Select=0 &\
+# $RadioGroup=0 &\
+# _linkSubmit= &\
+# $Submit=Next
+payload_page5 = {
+    'service': 'direct/1/SetupUserSource/$Form',
+    'sp': 'S0',
+    'Form0': '$RadioGroup,$Select,$LinkSubmit,$Submit,$Submit$0',
+    '$Select': '0',
+    '$RadioGroup': '0',
+    '$_linkSubmit': '',
+    '$Submit': 'Next'
+}
+
+# Setup Page 6 - SetupCompleted
+# service=direct/1/SetupVerify/$Form &\
+# sp=S0 &\
+# Form0=$Submit,$Submit$0 &\
+# $Submit=Confirm
+payload_page6 = {
+    'service': 'direct/1/SetupVerify/$Form',
+    'sp': 'S0',
+    'Form0': '$Submit,$Submit$0',
+    '$Submit': 'Confirm' 
+}
+
+### End default vars/config
+
+
+# GET /app?service=page/SetupAdmin
+
+
+# POST /app
